f3690e0c4e358dc139bcd21d480145e5602302d9
m3ggle <megglebande@web.de>
2023-07-10 11:05:26 +0200

    modules to standalone components

--- src/app/pages/gallery/pokemon-list/pokemon-item/pokemon-item.component.ts
+++ src/app/pages/gallery/pokemon-list/pokemon-item/pokemon-item.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LazyLoadImageModule } from 'src/app/directives/lazy-load-image/lazy-load-image.module';
 import { Pokemon } from 'src/app/interfaces/pokemon.interfaces';
 
 @Component({
   selector: 'app-pokemon-item',
   templateUrl: './pokemon-item.component.html',
+  standalone: true,
+  imports: [CommonModule, LazyLoadImageModule, RouterLink]
 })
 export class PokemonItemComponent {
   @Input("pokemonData") pokemon!: Pokemon
--- src/app/pages/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BackToTopComponent } from 'src/app/generalComponents/back-to-top/back-to-top.component';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
+  standalone: true,
+  imports: [CommonModule, BackToTopComponent]
 })
 export class PokemonDetailComponent {
 
